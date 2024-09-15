93c0b0e414b99f9677c1f945e5c73eab01f5e3a0
nashthecoder <naijeria@gmail.com>
2024-09-15 16:58:02 +0300

    fixed linkt errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'settings', to: 'settings#index'
+  get "settings", to: "settings#index"
   get "users/profile"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
