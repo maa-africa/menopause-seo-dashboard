fa9880b59e5e8a4d0edbc2a59b42ee03c81398a3
nashthecoder <naijeria@gmail.com>
2024-09-28 01:19:08 +0300

    Add error handling for HTTP requests in SerpapiService

--- app/services/serpapi_service.rb
+++ app/services/serpapi_service.rb
@@ -1,23 +1,41 @@
 # app/services/serpapi_service.rb
+require 'uri'
+require 'net/http'
+require 'json'
+
 class SerpapiService
   def initialize
-    @client = GoogleSearchResults.new(api_key: ENV["SERPAPI_API_KEY"]) # Make sure the API key is correct
+    @url = URI("https://api.serphouse.com/serp/live")
+    @http = Net::HTTP.new(@url.host, @url.port)
+    @http.use_ssl = true
+    @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    @headers = {
+      "accept" => 'application/json',
+      "content-type" => 'application/json',
+      "authorization" => "Bearer #{ENV['SERPHOUSE_API_TOKEN']}"
+    }
   end
 
   def search_keyword_ranking(keyword)
     Rails.logger.debug("Searching SERP for keyword: #{keyword}")
-    
-    begin
-      response = @client.get_json({
+
+    request = Net::HTTP::Post.new(@url, @headers)
+    request.body = {
+      data: {
+        domain: "google.com",
+        lang: "en",
         q: keyword,
-        engine: "google",
-        google_domain: "google.com",
-        hl: "en"
-      })
+        loc: "Texas,United States",
+        device: "desktop",
+        serp_type: "web"
+      }
+    }.to_json
 
-      Rails.logger.debug("SERP API response: #{response.inspect}")
+    begin
+      response = @http.request(request)
+      Rails.logger.debug("SERP API response: #{response.body}")
 
-      return parse_results(response)
+      return parse_results(JSON.parse(response.body))
     rescue StandardError => e
       Rails.logger.error("Error fetching SERP data: #{e.message}")
       return []
